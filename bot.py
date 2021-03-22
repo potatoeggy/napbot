02501d7a0821c3db10690f20c4f4335f7b094fb5
eggy <25178974+potatoeggy@users.noreply.github.com>
2021-03-22 11:17:33 -0400

    increase delay to 10 ms

--- bot.py
+++ bot.py
@@ -164,7 +164,7 @@ class LyricPlayer():
 			try:
 				end_stamp = s.index("]")
 				time_string = s[1:end_stamp]
-				time_string_ms = sum(x * int(t) for x, t in zip([0.001, 1, 60], reversed(re.split(":|\.", time_string))))-0.005
+				time_string_ms = sum(x * int(t) for x, t in zip([0.001, 1, 60], reversed(re.split(":|\.", time_string))))-0.010
 				if not s[end_stamp+1:].isspace():
 					print("a" + s + "b")
 					self.lyrics.append((time_string_ms - time_delta, s[end_stamp+1:]))
