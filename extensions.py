e7cab5cdb10a9bbd7db2c1bf74e0b67b8b2a951e
eggy <25178974+potatoeggy@users.noreply.github.com>
2021-04-01 12:12:54 -0400

    archive_status: debug

--- extensions.py
+++ extensions.py
@@ -10,10 +10,11 @@ class archive_status(commands.Cog):
     async def on_member_update(self, before, after):
         if before.status == discord.Status.offline: # if status was not custom changed
             return
-        a = str(next(filter(lambda i: i.type == discord.ActivityType.custom, after.activities), None)))
-        b = str(next(filter(lambda i: i.type == discord.ActivityType.custom, before.activities), None)))
+        a = str(next(filter(lambda i: i.type == discord.ActivityType.custom, after.activities), None))
+        b = str(next(filter(lambda i: i.type == discord.ActivityType.custom, before.activities), None))
         if a == "" or b == a: # if it was cleared or if it hasn't changed
             return
+        print(b,a)
         embed = discord.Embed(description=a)
         embed.set_author(name=str(after), icon_url=after.avatar_url)
         await self.bot.get_channel(self.embed_channel).send(embed=embed)
