e60d7d6b149405515ebed3f4f15f70847d7c07dc
eggy <25178974+potatoeggy@users.noreply.github.com>
2021-06-03 22:31:43 -0400

    music: don't bounce the line count around so much

--- bot.py
+++ bot.py
@@ -186,14 +186,14 @@ class LyricPlayer():
 		
 		MAX_LINES = 10
 		embed = discord.Embed(title=self.title)
-		embed.description = "\n".join(self.lyrics[:min(MAX_LINES*2+1, len(self.lyrics))])
+		embed.description = "\n".join(self.lyrics[:min(MAX_LINES+1, len(self.lyrics))])
 		msg = await self.channel.send(embed=embed)
 
 		start = time.time()
 		
 		for i, t in enumerate(self.lyric_times):
 			now = time.time()
-			embed.description = "\n".join(self.lyrics[max(0, i-MAX_LINES):i] + [f"**{self.lyrics[i]}**"] + self.lyrics[i+1:min(i+MAX_LINES+1, len(self.lyrics))] if i < len(self.lyrics)-1 else [])
+			embed.description = "\n".join(self.lyrics[max(0, i-MAX_LINES):i] + [f"**{self.lyrics[i]}**"] + (self.lyrics[i+1:min(i+MAX_LINES+1, len(self.lyrics))] if i < len(self.lyrics)-1 else []))
 			while now < t + start:
 				if not self.running:
 					return
